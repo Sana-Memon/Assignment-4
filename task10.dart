02085e2212377af08c19b6a12b2475f32dcf572c
Sanaa-hub <sanaamemon567@gmail.com>
2022-11-07 00:07:24 +0500

    Updated files 3

--- task10.dart
+++ task10.dart
@@ -1,3 +1,9 @@
 void main() {
   String myString = 'Jawan Pakistan';
+  var reversed = StringBuffer();
+  for (var i = myString.length - 1; i >= 0; --i) {
+    reversed.write(myString[i]);
+  }
+
+  print(reversed);
 }
